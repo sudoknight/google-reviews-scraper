6d1d2a06ef9416303307c366ef4f738bd59fd99b
Hassan Ghalib <hassanghalib@192.168.1.69>
2024-03-08 12:15:52 +0500

    updated run_as_module

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 from typing_extensions import Annotated
 
 from core.data_models import Input
-from core.scrape import run
+from core.scrape import execute_search_term_on_google, execute_visit_google_url
 
 app = typer.Typer()
 
@@ -74,17 +74,20 @@ def execute(
 
         input_params["stop_critera"] = stop
 
-    input_params = Input(**input_params)
+    input_obj = Input(**input_params)
+    ls_reviews = [] 
+    overall_rating = {}
     with sync_playwright() as playwright:
-        ls_reviews = run(playwright, input_params)
+        ls_reviews, overall_rating = execute_search_term_on_google(playwright, input_obj)
         print(f"Scrapping Complete: Total Reviews  {len(ls_reviews)}")
 
 
 def run_as_module(
-    search_term: str,
-    sort_by: str,
-    n_reviews: int,
-    save_to_disk: bool,
+    google_page_url: str = "",
+    search_term: str = "",
+    sort_by: str = "most_recent",
+    n_reviews: int = -1,
+    save_to_disk: bool = True,
     stop_cri_user: str = "",
     stop_cri_review: str = "",
 ) -> Tuple[List[dict], dict]:
@@ -98,14 +101,23 @@ def run_as_module(
     """
     ls_res: List[dict] = []
     overall_rating: dict = {}
+    if not len(google_page_url) and not len(search_term):
+        raise Exception(
+            "Pass atleast one argument from 'google_page_url' and 'search_term' "
+        )
 
     input_params = {
-        "search_term": search_term,
         "sort_by": sort_by,
         "n_reviews": n_reviews,
         "save_review_to_disk": True if save_to_disk else False,
         "save_metadata_to_disk": True if save_to_disk else False,
     }
+    if len(search_term):
+        print("Google Search Term Received")
+        input_params["search_term"] = search_term
+    else:
+        print("Google Page Url Received")
+        input_params["google_page_url"] = google_page_url
 
     if stop_cri_user:
         stop = {"username": stop_cri_user}
@@ -115,9 +127,17 @@ def run_as_module(
 
         input_params["stop_critera"] = stop
 
-    input_params = Input(**input_params)
+    input_obj = Input(**input_params)
     with sync_playwright() as playwright:
-        ls_res, overall_rating = run(playwright, input_params)
+        if len(search_term):
+            print("Calling execute_search_term_on_google")
+            ls_res, overall_rating = execute_search_term_on_google(
+                playwright, input_obj
+            )
+        else:
+            print("Calling execute_visit_google_url")
+            ls_res, overall_rating = execute_visit_google_url(playwright, input_obj)
+
         print(f"Scrapping Complete: Total Reviews  {len(ls_res)}")
 
     return ls_res, overall_rating
@@ -125,3 +145,6 @@ def run_as_module(
 
 if __name__ == "__main__":
     typer.run(execute)
+    # run_as_module(
+    #     google_page_url="https://www.google.com/travel/search?q=all%20hotels%20in%20doha%2C%20qatar&g2lb=2502548%2C2503771%2C2503781%2C2504375%2C4258168%2C4284970%2C4291517%2C4597339%2C4814050%2C4874190%2C4893075%2C4924070%2C4965990%2C4969802%2C10207535%2C10208620%2C72277293%2C72298667%2C72302247%2C72313836%2C72317059%2C72406588%2C72412680%2C72414906%2C72421566%2C72430562%2C72440516%2C72442338%2C72458707%2C72466827%2C72470899%2C72471395&hl=en-PK&gl=pk&ssta=1&ts=CAESCgoCCAMKAggDEAAaTQovEi0yJTB4M2U0NWM1MzRmZmRjZTg3ZjoweDQ0ZDkzMTlmNzhjZmQ0YjE6BERvaGESGhIUCgcI6A8QARgOEgcI6A8QARgPGAEyAhAAKgcKBToDUEtS&qs=CAESBENEWT0yJkNoZ0k0cGF4MlBlQm9mYThBUm9MTDJjdk1YUm9jemQwZW1NUUFROAZCCQkJr3mcvWa-gEIJCc1CagK22cKNQgkJgGr6UZAGJfNIAA&ap=MAFoAQ&ictx=1&sa=X&ved=0CMcBEMr3BGoXChMIqMjUmsrTgwMVAAAAAB0AAAAAEEw",
+    # )
